21feb1f5546050d6c81642b65ecb456e64c20cd9
sps-dev <sps-dev@login1.cluster.net>
2023-12-12 10:50:30 +0100

    modified:   util/SPS4_hindcast_checklist.sh

--- src/util/SPS4_hindcast_checklist.sh
+++ src/util/SPS4_hindcast_checklist.sh
@@ -13,6 +13,7 @@ exec 3>&1 1>>${LOG_FILE} 2>&1
 
 typeofrun="hindcast"
 hindcasts_list=${SPSSystem}_${typeofrun}_list.csv
+listfiletocheck="deleteme.csv"
 #copy the relative file from Zeus
 if [[ $machine == "juno" ]]
 then
@@ -31,8 +32,7 @@ else
 fi
 listofcases=`ls|grep ${SPSSystem}_[12]`
 cd $DIR_CASES
-listfiletocheck="deleteme.csv"
-cp ${DIR_CHECK}/${hindcasts_list} ${DIR_CHECK}/$listfiletocheck
+cp ${DIR_CHECK}/${hindcasts_list} ${DIR_TEMP}/$listfiletocheck
 for caso in $listofcases 
 do
   if [[ ! -d $DIR_CASES/$caso ]] ; then
@@ -44,19 +44,19 @@ do
      continue
   fi
   # find line number
-  LN="$(grep -n "$caso" ${DIR_CHECK}/$listfiletocheck | cut -d: -f1)"
+  LN="$(grep -n "$caso" ${DIR_TEMP}/$listfiletocheck | cut -d: -f1)"
 
   for i in `seq 1 $ndone`
   do
      # calc position of the ith-column inside csv table
      table_column_id=$(($i + 1))
 # assign a value with -val selecting a row with -v and a column with -c
-     awk -v r=$LN -v c=$table_column_id -v val='1' 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_CHECK}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
+     awk -v r=$LN -v c=$table_column_id -v val='1' 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_TEMP}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
 
 # add 1 second wait to be sure the file has been modified
      sleep 1
 
-     mv -f $DIR_TEMP/$listfiletocheck.tmp1 ${DIR_CHECK}/$listfiletocheck
+     mv -f $DIR_TEMP/$listfiletocheck.tmp1 ${DIR_TEMP}/$listfiletocheck
 
   done
   CASEROOT=$DIR_CASES/$caso
@@ -68,31 +68,32 @@ do
   then
      table_column_id=$(($table_column_id + 1))
 # assign a value with -val selecting a row with -v and a column with -c
-     awk -v r=$LN -v c=$table_column_id -v val='1' 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_CHECK}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
+     awk -v r=$LN -v c=$table_column_id -v val='1' 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_TEMP}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
 # add 1 second wait to be sure the file has been modified
      n_complete=$(($n_complete + 1))
      sleep 1
 
-     mv -f $DIR_TEMP/$listfiletocheck.tmp1 ${DIR_CHECK}/$listfiletocheck
+     mv -f $DIR_TEMP/$listfiletocheck.tmp1 ${DIR_TEMP}/$listfiletocheck
   fi
   if [[ -f $check_pp_C3S ]]
   then
 # assign a value with -val selecting a row with -v and a column with -c
-     awk -v r=$LN -v c=$table_column_id_ndays -v val='1' 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_CHECK}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
+     awk -v r=$LN -v c=$table_column_id_ndays -v val='1' 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_TEMP}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
 # add 1 second wait to be sure the file has been modified
      sleep 1
 
-     mv -f $DIR_TEMP/$listfiletocheck.tmp1 ${DIR_CHECK}/$listfiletocheck
+     mv -f $DIR_TEMP/$listfiletocheck.tmp1 ${DIR_TEMP}/$listfiletocheck
   fi
   
 done
-awk -v r=2 -v c=$table_column_id_ndays -v val="$n_complete" 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_CHECK}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
+awk -v r=2 -v c=$table_column_id_ndays -v val="$n_complete" 'BEGIN{FS=OFS=","} NR==r{$c=val} 1' ${DIR_TEMP}/$listfiletocheck > $DIR_TEMP/$listfiletocheck.tmp1
 sleep 1
 if [[ ! -f ${DIR_TEMP}/$listfiletocheck.tmp1 ]]
 then
    echo "not found"
 fi
 mv ${DIR_TEMP}/$listfiletocheck.tmp1 ${DIR_CHECK}/${hindcasts_list}
+rm ${DIR_TEMP}/$listfiletocheck
 
 if [[ $machine == "juno" ]]
 then
