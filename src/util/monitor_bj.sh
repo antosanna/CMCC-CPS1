da690727e4def1d59e238f5c0bb20b270af3e55d
cp1 project account <cp1@login1.juno.cmcc.scc>
2023-12-16 12:35:57 +0100

    new file:   util/monitor_bj.sh             to check production (sen email if runs < maxnumbertosubmit)

---
+++ src/util/monitor_bj.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+# load variables from descriptor
+. $HOME/.bashrc
+. ${DIR_UTIL}/descr_CPS.sh
+
+set -eu #vx
+
+job_run=`$DIR_UTIL/findjobs.sh -m $machine -n run.${SPSSystem}_ -a $BATCHRUN -c yes`
+job_pen=`$DIR_UTIL/findjobs.sh -m $machine -n run.${SPSSystem}_ -a $BATCHPEND -c yes`
+
+if [[ $job_run -lt $maxnumbertosubmit ]]
+then
+   title="[SPS4 HINDCASTS] WARNING!!! less then expected jobs"
+   body="Found $job_run job running instead of the expected $maxnumbertosubmit \n
+ and $job_pen job pending \n
+   \n
+   Please check if there is anything wrong \n
+\n"
+   ${DIR_UTIL}/sendmail.sh -m $machine -e $mymail -M "$body" -t "$title"
+fi
+exit 0
+
