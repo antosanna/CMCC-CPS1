fbf69da1876bc07f4cc1035067d942dd05c9b32c
cp1 project account <cp1@login1.juno.cmcc.scc>
2024-02-01 13:51:52 +0100

    modified:   facilities.txt             intriduced a few facilities for monitoring

--- src/recover/recover_interrupted.sh
+++ src/recover/recover_interrupted.sh
@@ -54,7 +54,7 @@ then
    . ${DIR_UTIL}/descr_ensemble.sh $yyyy
 fi
 
-debug=2  #set to 2 the first time you run in order to print only the list of interrupted 
+debug=0  #set to 2 the first time you run in order to print only the list of interrupted 
          #set to 1 the second time you run in order to process only one case for category
          #set to 0 to run all interrupted identified
 
@@ -252,7 +252,7 @@ then
    for caso in $lista_regrid_ice
    do
 #echo "first month,resubmit,lt_archive,lt_archive_moredays,regrid_ice,regrid_oce,postproc C3S" > $filecsv
-      echo "-,-,-,-,$caso,-,-,- " >> $filecsv
+      echo "-,-,-,-,$caso,-,- " >> $filecsv
    done
 fi
 if [[ "$lista_regrid_oce" != " " ]]
@@ -263,7 +263,7 @@ then
    for caso in $lista_regrid_oce
    do
 #echo "first month,resubmit,lt_archive,lt_archive_moredays,regrid_ice,regrid_oce,postproc C3S" > $filecsv
-      echo "-,-,-,-,-,$caso,-,- " >> $filecsv
+      echo "-,-,-,-,-,$caso,- " >> $filecsv
    done
 fi
 if [[ "$lista_pp_C3S" != " " ]]
