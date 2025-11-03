682645cbc53ad0173171f9c226578c3b7ae0c90b
cp2 project account <cp2@login1.cassandra.cmcc.scc>
2025-11-03 14:17:55 +0100

    Updates and new script for mrlsl (missing in postproc) new file:   ../C3S_standard/postproc_CERISE_phase2_offline_mrlsl.sh modified:   CERISE_table_clm.txt new file:   CERISE_table_clm_mrlsl.txt new file:   postpc_clm_CERISE_mrlsl.sh

---
+++ src/postproc/C3S_standard/postproc_CERISE_phase2_offline_mrlsl.sh
@@ -0,0 +1,71 @@
+#!/bin/sh -l 
+
+# load variables from descriptor
+. $HOME/.bashrc
+. ${DIR_UTIL}/descr_CPS.sh
+. ${DIR_UTIL}/load_nco                # to get command $compress
+. ${DIR_UTIL}/descr_ensemble.sh $1  #THIS SCRIPT SHOULD RUN
+                                      #ONLY FOR HINDCASTS
+
+set -evxu
+
+caso=$2
+# only in this special case DIR_CASES must be redefined for it can be different
+# for the different machines the case could have been done on.
+dir_cases=$3
+# this modification will affect $dictionary too!!!!
+
+st=`echo $caso|cut -d '_' -f2 |cut -c5-6`
+yyyy=`echo $caso|cut -d '_' -f2 |cut -c1-4`
+#
+startdate=$yyyy$st
+ens=`echo $caso|cut -d '_' -f 3 `
+member=`echo $ens|cut -c2,3` 
+
+HEALED_DIR=$HEALED_DIR_ROOT/$caso
+#HEALED_DIR_ROOT=/work/cmcc/cp1/CPS/CMCC-CPS1/fixed_from_spikes/
+# THIS MUST BE KEPT FOR CERISE
+chmod -R u+w $DIR_ARCHIVE/$caso
+
+outdirC3S=${WORK_CERISE}/$yyyy$st/
+
+set +euvx
+. $dictionary
+set -euvx
+mkdir -p $outdirC3S
+dirlog=$dir_cases/$caso/logs
+mkdir -p $dirlog
+
+#***********************************************************************
+# Standardization for CLM
+#***********************************************************************
+wkdir_clm=$SCRATCHDIR/regrid_CERISE_phase2/$caso/CLM
+mkdir -p ${wkdir_clm}
+# get check_postclm  from dictionary
+
+if [[ ! -f $check_all_postclm ]]
+then
+
+   cd ${wkdir_clm}
+   filetyp="h1"
+   jobIDall=""
+   for ft in $filetyp ; do
+
+       case $ft in
+           h1) mult=1 ; req_mem=50000 ;;
+           h2) mult=4 ; req_mem=25000 ;;
+           h3) mult=1 ; req_mem=5000;; # for land both h1 and h3 are daily (h1 averaged and h3 instantaneous), multiplier=1
+       esac
+       flag_for_type=${check_postclm_type}_${ft}_DONE
+       finalfile_clm=$DIR_ARCHIVE/$caso/lnd/hist/$caso.clm2.$ft.$yyyy-$st.zip.nc
+       echo "start of postpc_clm "`date`
+       finalfile_clm=$DIR_ARCHIVE/$caso/lnd/hist/$caso.clm2.$ft.$yyyy-$st.zip.nc
+       input="${finalfile_clm} $ens $startdate $outdirC3S $caso ${flag_for_type} ${wkdir_clm} $ic $ft"
+       # ADD the reservation for serial !!!
+       ${DIR_UTIL}/submitcommand.sh -m $machine -q $parallelq_l -M ${req_mem} -p create_clm_files_${ft}_${caso} -S qos_resv -j postpc_clm_${ft}_${caso} -l $dir_cases/$caso/logs/ -d ${DIR_POST}/clm -s postpc_clm_CERISE_mrlsl.sh -i "$input"
+   done
+fi
+
+echo "Done."
+
+exit 0
