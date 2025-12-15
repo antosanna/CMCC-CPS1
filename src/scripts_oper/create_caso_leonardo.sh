2f646ff56753ed3407a2416e33eeb17dcb7e36c0
Antonella Sanna <a07cmc00@login05.leonardo.local>
2025-12-15 15:19:31 +0100

    modified:   create_caso_leonardo.sh     with the new refcase in CMCC_2025 instead of CMCC_COPERN_4

--- src/scripts_oper/create_caso_leonardo.sh
+++ src/scripts_oper/create_caso_leonardo.sh
@@ -45,12 +45,13 @@ $DIR_UTIL/clean_caso.sh $caso
 set +euvx
 # from 20251029 /leonardo_work/CMCC_Copernic_4 merged to /leonardo_work/CMCC_2025 and  removed (refcases re-created on $DIR_CASES) 
 #20251031 - some problem with new refcase - getting back to the old one
-if [[ $machine == "leonardo" ]]
-then
-   $DIR_CESM/cime/scripts/create_clone --case $DIR_CASES/$caso --clone /leonardo_work/CMCC_Copernic_4/CPS/CMCC-CPS1/cases/$refcase --cime-output-root $WORK_CPS
-else
+# FIXED 20251111
+#if [[ $machine == "leonardo" ]]
+#then
+#   $DIR_CESM/cime/scripts/create_clone --case $DIR_CASES/$caso --clone /leonardo_work/CMCC_Copernic_4/CPS/CMCC-CPS1/cases/$refcase --cime-output-root $WORK_CPS
+#else
    $DIR_CESM/cime/scripts/create_clone --case $DIR_CASES/$caso --clone $DIR_CASES1/$refcase --cime-output-root $WORK_CPS
-fi
+#fi
 
 set -euvx
 #----------------------------------------------------------
@@ -156,6 +157,7 @@ if [[ $yyyy$st -eq 201501 ]]
 then   
    echo "use_init_interp = .true." >> $DIR_CASES/$caso/user_nl_clm
 fi
+
 
 cp $DIR_TEMPL/user_nl_hydros $DIR_CASES/$caso
 #----------------------------------------------------------
